3af8c93297295d3a8248067607690ad1dd74cbe0
Jun Woo Shin <shin.junwoo97@gmail.com>
2017-05-03 00:43:25 -0400

    added rules

--- main.hs
+++ main.hs
@@ -1,3 +1,11 @@
 module Main (main) where
 
-main = print "hi"
+-- Rules
+-- Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+-- Any live cell with two or three live neighbours lives on to the next generation.
+-- Any live cell with more than three live neighbours dies, as if by overpopulation.
+-- Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+grid x y = [[0 | y' <- [1..y]] | x' <- [1..x]]
+
+main = print $ grid 100 100
