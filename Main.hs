e6e6a0565f0ebd4e4223f2ab435ca40fcac24496
Jun Woo Shin <shin.junwoo97@gmail.com>
2017-08-12 18:19:12 +0000

    cleanup

--- Main.hs
+++ Main.hs
@@ -1,12 +1,16 @@
 module Main where
 
+import Data.Array
+
+
 -- Rules
 -- Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
 -- Any live cell with two or three live neighbours lives on to the next generation.
 -- Any live cell with more than three live neighbours dies, as if by overpopulation.
 -- Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
+grid :: (Integral a) => a -> a -> [[a]]
 grid x y = [[0 | y' <- [1..y]] | x' <- [1..x]]
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = putStrLn "hello"
